d1a105c151c62b28b9b73d6d3b5a2e286a3882dc
slusarczykmichal <slusarczyk.michal@outlook.com>
2026-01-28 09:23:48 +0100

    rework filtering query

--- packages/modules/b2c-core/src/api/admin/middlewares.ts
+++ packages/modules/b2c-core/src/api/admin/middlewares.ts
@@ -3,12 +3,11 @@ import { MiddlewareRoute } from '@medusajs/framework';
 import { attributeMiddlewares } from './attributes/middlewares';
 import { configurationMiddleware } from './configuration/middlewares';
 import { adminCustomMiddlewares } from './custom/middlewares';
-import { adminInvMiddlewares } from './inventory-items-vendor/middlewares';
+import { adminVendorInventoryMiddlewares } from './inventory-items-vendor/middlewares';
 import { orderSetsMiddlewares } from './order-sets/middlewares';
 import { adminOrdersMiddlewares } from './orders/middlewares';
 import { adminProductsMiddlewares } from './products/middlewares';
-import { adminReservationsVendorMiddlewares } from './reservations-vendor/middlewares';
-import { adminReservationsMiddlewares } from './reservations/middlewares';
+import { adminVendorReservationsMiddlewares } from './reservations-vendor/middlewares';
 import { sellerMiddlewares } from './sellers/middlewares';
 
 export const adminMiddlewares: MiddlewareRoute[] = [
@@ -19,7 +18,6 @@ export const adminMiddlewares: MiddlewareRoute[] = [
   ...adminProductsMiddlewares,
   ...adminCustomMiddlewares,
   ...adminOrdersMiddlewares,
-  ...adminReservationsMiddlewares,
-  ...adminInvMiddlewares,
-  ...adminReservationsVendorMiddlewares
+  ...adminVendorInventoryMiddlewares,
+  ...adminVendorReservationsMiddlewares
 ];
